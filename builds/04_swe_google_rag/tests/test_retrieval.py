c520bf9b774ec62d086a6558c37abdbe6449f06b
Jan Haag <jan.valentin.haag@gmail.com>
2026-07-30 08:10:45 +0200

    add: init test for retrieval

---
+++ builds/04_swe_google_rag/tests/test_retrieval.py
@@ -0,0 +1,4 @@
+"""Placeholder tests for vector compatibility and similarity retrieval."""
+
+# TODO: Use small known vectors to test ranking, top_k, dimension errors, ties,
+# and source metadata without making external API calls.
