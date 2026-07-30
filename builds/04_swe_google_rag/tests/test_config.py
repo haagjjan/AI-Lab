ab86b0993fb7aaa28702b7321ce19ba35629ff9e
Jan Haag <jan.valentin.haag@gmail.com>
2026-07-30 08:10:34 +0200

    add: init test for config

---
+++ builds/04_swe_google_rag/tests/test_config.py
@@ -0,0 +1,4 @@
+"""Placeholder tests for configuration loading and validation."""
+
+# TODO: Test required settings, numeric validation, relative paths, and secret-
+# safe error messages when configuration loading is implemented.
